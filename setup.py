9eb9f0b727babed427863bd421d9603879f12a13
Taito Horiuchi <taito.horiuchi@gmail.com>
2012-05-14 22:05:30 +0300

    Version updated.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='sll.templates',
-    version='0.3',
+    version='0.4',
     description="Collection of SLL Templates",
     long_description=open("README.rst").read(),
     # Get more strings from
